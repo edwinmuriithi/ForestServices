ee27cd7e17869ebd1cb8683a3199d69f510b63e3
wdmuthiora <wdmuthiora@gmail.com>
2021-11-11 22:49:39 +0300

    Test that Animal initializes correctly with name.

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,4 +1,10 @@
 public class Animal {
+    private String name;
+    private String type;
     public Animal(String name, String type) {
+        this.name=name;
+    }
+    public String getName(){
+        return name;
     }
 }
--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -4,12 +4,23 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AnimalTest {
+
+
     @Test
     @DisplayName("Animal Instantiates Correctly.")
     public void testInstanceOfAnimal_true(){
         Animal testAnimal = setUpNewAnimal();
         assertEquals(true, testAnimal instanceof Animal);
     }
+
+    @Test
+    @DisplayName("Animal Instantiates Correctly the name Betty.")
+    public void getName_animalInstantiatesWithName_Betty() {
+        Animal testAnimal = setUpNewAnimal();
+        assertEquals("Betty", testAnimal.getName());
+    }
+
+
     //helper method.
     private Animal setUpNewAnimal() {
         return new Animal ("Betty", "cow");
