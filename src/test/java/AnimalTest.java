2b0f5a0a8cfd117194a5ba0d08e528e1607db314
wdmuthiora <wdmuthiora@gmail.com>
2021-11-11 22:38:01 +0300

    Test that Animal initializes correctly.

--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -1,9 +1,14 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AnimalTest {
     @Test
+    @DisplayName("Animal Instantiates Correctly.")
     public void testInstanceOfAnimal_true(){
         Animal testAnimal = setUpNewAnimal();
+        assertEquals(true, testAnimal instanceof Animal);
     }
     //helper method.
     private Animal setUpNewAnimal() {
