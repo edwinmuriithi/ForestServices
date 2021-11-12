a1e59a9df411cd75d221be1dde0ca8d23f644d8a
wdmuthiora <wdmuthiora@gmail.com>
2021-11-12 21:34:05 +0300

    Tested and Implemented Sighting instantiating properly.

--- src/main/java/Sighting.java
+++ src/main/java/Sighting.java
@@ -1,2 +1,4 @@
 public class Sighting {
+    public Sighting(int i, int animalId, int rangerId, int locationId) {
+    }
 }
--- src/test/java/SightingTest.java
+++ src/test/java/SightingTest.java
@@ -1,2 +1,57 @@
+import org.junit.Rule;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.sql2o.Connection;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SightingTest {
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
+
+    @AfterEach
+    protected void after() {
+        //clear animals table after each test
+        try(Connection con = DB.sql2o.open()) {
+            String deleteAnimalsQuery = "DELETE FROM animals;";
+            con.createQuery(deleteAnimalsQuery).executeUpdate();
+        }
+    }
+
+    @Test
+    @DisplayName("Sighting Instantiates Correctly.")
+    public void testInstanceOfAnimal_true(){
+        Sighting testSighting = setUpNewSighting();
+        assertEquals(true, testSighting instanceof Sighting);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //helper method.
+    private Sighting setUpNewSighting() {
+        return new Sighting (1,1,1, 1);
+    }
 }
