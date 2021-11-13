2df453dcd84b5fe4a0bb371d780131a542b3c0a1
wdmuthiora <wdmuthiora@gmail.com>
2021-11-13 22:07:26 +0300

    Created Ranger-creation form.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,12 +6,29 @@ import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
+        staticFileLocation("/public");
+
+        //home
         get("/",(request, response) -> {
-            Map<String,Object> model=new HashMap<String, Object>();
+            Map<String,Object> model = new HashMap<String, Object>();
             return new ModelAndView(model,"index.hbs");
         },new HandlebarsTemplateEngine());
 
+        //navigate ranger creation form
+        get("/create/ranger",(request, response) -> {
+            Map<String,Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model,"ranger-form.hbs");
+        },new HandlebarsTemplateEngine());
 
+        //create a ranger.
+        post("/create/ranger/new",(request, response) -> {
+            Map<String,Object> model = new HashMap<String, Object>();
+            String name = request.queryParams("name");
+            String badge_number = request.queryParams("badge");
+            Ranger ranger = new Ranger(name, badge_number);
+            ranger.save();
+            return new ModelAndView(model,"ranger-form.hbs");
+        },new HandlebarsTemplateEngine());
 
 
 
